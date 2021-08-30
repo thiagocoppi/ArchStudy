e8da50d027b92495cb285574018e87a90f1d91a7
Thiago Coppi da Silva (Premier) <t0032583@ailos.coop.br>
2021-08-30 12:43:03 -0300

    Ajuste test

--- Tests/Associados/AssociadosServiceTests.cs
+++ Tests/Associados/AssociadosServiceTests.cs
@@ -10,46 +10,34 @@ namespace Tests.Associados
 {
     public class AssociadosServiceTests : BaseTestIntegration
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
 
         [Test]
         public void Dado_AssociadoMenorDeIdade_Deve_AdicionarNotificacaoDeErro()
         {
-            using (var scope = StartupTest().CreateScope())
-            {
-                var _associadoService = scope.ServiceProvider.GetService<IAssociadoService>();
-                var _notificationContext = scope.ServiceProvider.GetService<INotificationContext>();
-                _associadoService.CadastrarAssociado(
-                    new Associado("Thiago Coppi", 17, "07052429942", new Endereco("Rua tapajos", 74, ""))).GetAwaiter().GetResult();
-                Assert.IsTrue(_notificationContext.HaveNotification());
-            }
+            using var scope = StartupTest().CreateScope();
+            var _associadoService = scope.ServiceProvider.GetService<IAssociadoService>();
+            var _notificationContext = scope.ServiceProvider.GetService<INotificationContext>();
+            _associadoService.CadastrarAssociado(
+                new Associado("Thiago Coppi", 17, "07052429942", new Endereco("Rua tapajos", 74, ""))).GetAwaiter().GetResult();
+            Assert.IsTrue(_notificationContext.HaveNotification());
         }
 
         [Test]
         public void Dado_AssociadoMaiorIdade_NaoDeve_AdicionarNotificacaoDeErro()
         {
-            using (var scope = StartupTest().CreateScope())
-            {
-                using (var factory = new SampleDbContextFactory())
-                {
-                    using (var context = factory.CreateContext())
-                    {
-                        context.Database.OpenConnection();
-                        context.Database.Migrate();
-                        var _database = scope.ServiceProvider.GetRequiredService<ArchContext>();
-                        _database.Database.MigrateAsync().GetAwaiter().GetResult();
-                        var _associadoService = scope.ServiceProvider.GetService<IAssociadoService>();
-                        var _notificationContext = scope.ServiceProvider.GetService<INotificationContext>();
-                        _associadoService.CadastrarAssociado(
-                            new Associado("Thiago Coppi", 21, "07052429942", new Endereco("Rua tapajos", 74, ""))).GetAwaiter().GetResult();
-                        Assert.IsFalse(_notificationContext.HaveNotification());
-                        context.Database.CloseConnection();
-                    }
-                }
-            }
+            using var scope = StartupTest().CreateScope();
+            using var factory = new SampleDbContextFactory();
+            using var context = factory.CreateContext();
+            context.Database.OpenConnection();
+            context.Database.Migrate();
+            var _database = scope.ServiceProvider.GetRequiredService<ArchContext>();
+            _database.Database.MigrateAsync().GetAwaiter().GetResult();
+            var _associadoService = scope.ServiceProvider.GetService<IAssociadoService>();
+            var _notificationContext = scope.ServiceProvider.GetService<INotificationContext>();
+            _associadoService.CadastrarAssociado(
+                new Associado("Thiago Coppi", 21, "07052429942", new Endereco("Rua tapajos", 74, ""))).GetAwaiter().GetResult();
+            Assert.IsFalse(_notificationContext.HaveNotification());
+            context.Database.CloseConnection();
         }
     }
 }
